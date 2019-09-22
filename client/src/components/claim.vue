a24076d780c5d20ae083de027296e81557a4daef
derazano <36336494+derazano@users.noreply.github.com>
2019-09-23 00:36:22 +0700

    edit ui

--- client/src/components/claim.vue
+++ client/src/components/claim.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app id="inspire">
+  <v-app id="inspire" :style="{ backgroundImage: 'url(' + require('@/assets/a.jpeg') + ')' }">
     <v-navigation-drawer
       v-model="drawer"
       app
@@ -26,14 +26,15 @@
 
     <v-app-bar
       app
-      color="blue"
+      color="yellow accent-4"
       dark
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>แบบเรียกร้องค่าสินไหมทดแทนค่ารักษาพยาบาล</v-toolbar-title>
     </v-app-bar>
 
-    <v-container  class="justify-center">
+    <div class="bgn">
+    <v-container nowrap  class="justify-center">
       <v-form v-model="valid" ref="form">
       <v-layout  row nowrap class="justify-center">
         <v-flex md12 xs12 lg12 xl3>
@@ -53,20 +54,18 @@
                   ></v-text-field>
               </v-flex>
             </div>
-            <div class="text-center">
+
              <v-flex md12 xs12 lg12 xl3>
-              <br />
-              <br />
-              <p lg12 xs3 v-if="customerCheck != ''">Customer Name : {{customerName}}</p>
+              <p  class="text-center" lg12 xs3 v-if="customerCheck != ''">Customer Name : {{customerName}}</p>
               </v-flex>
-            </div> 
             
-              <v-flex md6 xs12 lg12 xl3>
-                  <v-btn @click="findCustomer" depressed large color="primary">Search</v-btn>
+             <div class="btncenter">
+              <v-flex md12 xs12 lg12 xl3>
+                  <v-btn class="justify-center" @click="findCustomer" depressed large color="primary">ค้นหา</v-btn>
               </v-flex>
 
-               
-               <v-flex md6 xs12 lg6 xl3>
+              <div v-if="customerCheck">
+               <v-flex md6 xs12 lg12 xl3>
                   <v-select
                     style="width: 300px"
                     label="ให้สั่งจ่ายเช็คในนามของ"
@@ -78,7 +77,7 @@
                   ></v-select>
                </v-flex>
        
-               <v-flex md6 xs12 lg4 xl3>
+               <v-flex md6 xs12 lg12 xl3>
                  <v-select
                   label="ได้รับการตรวจโดยวิธี"
                   style="width: 300px"
@@ -91,7 +90,7 @@
                 ></v-select>
                </v-flex>
           
-                <v-flex md6 xs12 lg6 xl3>
+                <v-flex md6 xs12 lg12 xl3>
                  <v-select
                   label="วิธีการรับการรักษา"
                   style="width: 300px"
@@ -104,7 +103,7 @@
                 ></v-select>
                 </v-flex>
 
-                <v-flex md6 xs12 lg4 xl3>
+                <v-flex md6 xs12 lg12 xl3>
                   <v-select
                   style="width: 300px"
                   label="แพ็คเก็จที่สมัครกับเรา"
@@ -116,7 +115,6 @@
                   required
                 ></v-select>
                 </v-flex>
-                <div class="btncenter">
                 <v-flex md6 xs12 lg12 xl3 >
                   <v-select
                   label="โรงพยาบาลที่เข้ารับการรักษา"
@@ -129,18 +127,19 @@
                   required
                 ></v-select>
                 </v-flex>
+                <div class="btncenter">
+            <v-col cols="12">
+              <v-btn large class="justify-center" md4 style="hight:300" depressed @click="saveclaim" :class="{ red: !valid, green: valid }">ส่งแบบฟอร์ม</v-btn>  
+            </v-col>
+            </div>
                 </div>
-      
+             </div>
       </v-layout>
      </v-form>
            
            
     </v-container>
-            <div class="btncenter">
-            <v-col cols="12">
-              <v-btn large class="justify-center" md4 style="hight:300" depressed color="primary">ส่งแบบฟอร์ม</v-btn>  
-            </v-col>
-            </div>
+    </div> 
   </v-app>
 </template>
 
@@ -256,6 +255,32 @@ import http from "../http-common";
       this.$refs.form.reset();
       this.customerCheck = false;
     },
+    saveClaim() {
+      http
+        .post(
+          "/Claim/" +
+            this.claim.customerId +
+            "/" +
+            this.claim.paychecksId +
+            "/" +
+            this.claim.treatmethodId +
+            "/" +
+            this.claim.hospitalId +
+            "/" +
+            this.claim.insuranceId +
+            "/" +
+            this.claim.curebyId,
+          this.claim
+        )
+        .then(response => {
+          console.log(response);
+          this.$router.push("/view");
+        })
+        .catch(e => {
+          console.log(e);
+        });
+      this.submitted = true;
+    },
     refreshList() {
       this.getPaycheckss();
       this.getTreatmethods();
@@ -284,4 +309,17 @@ import http from "../http-common";
     margin-left: auto;
     margin-right: auto;
 }
+.bgn{
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    height: 1080px;
+    width: 1000px; 
+    background-color: #c88d8b;
+    opacity: 0.9;
+    -moz-box-shadow:inset 0 0 10px #000000;
+   -webkit-box-shadow:inset 0 0 10px #000000;
+   box-shadow:inset 0 0 10px #000000;
+}
+
 </style>
